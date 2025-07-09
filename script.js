415af3fc5955ef1377bb4a91f45efa10d7487181
oisiiomoti <k24as011@st.kyusan-u.ac.jp>
2025-07-09 17:23:05 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // JavaScript
-const open = document.querySelector('#btn-open');
-const close = document.querySelector('#btn-close');
-const menu = document.querySelector('#menu');
+// const open = document.querySelector('#btn-open');
+// const close = document.querySelector('#btn-close');
+// const menu = document.querySelector('#menu');
 // const items = document.querySelectorAll('.item');
 
-const options = {
-  duration: 600,
-  easing: 'ease',
-  fill: 'forwards',
-}
+// const options = {
+//   duration: 600,
+//   easing: 'ease',
+//   fill: 'forwards',
+// }
 
-const menuOpen = () => {
-  const keyframes = {
-    visibility: ['hidden','visible'],
-    opacity: [0,1]
-  }
-  menu.animate(keyframes,options);
+// const menuOpen = () => {
+//   const keyframes = {
+//     visibility: ['hidden','visible'],
+//     opacity: [0,1]
+//   }
+//   menu.animate(keyframes,options);
 
 //   items.forEach((item,index) => {
 //     item.animate({opacity:[0,1]},{
@@ -26,15 +26,15 @@ const menuOpen = () => {
 //     });
 //   });
 
-}
-open.addEventListener('click',menuOpen);
+// }
+// open.addEventListener('click',menuOpen);
 
-const menuClose = () => {
-  const keyframes = {
-    visibility: ['visible','hidden'],
-    opacity: [1,0]
-  }
-  menu.animate(keyframes,options);
+// const menuClose = () => {
+//   const keyframes = {
+//     visibility: ['visible','hidden'],
+//     opacity: [1,0]
+//   }
+//   menu.animate(keyframes,options);
 
 //   items.forEach((item) => {
 //     item.animate({opacity:[1,0]},{
@@ -44,17 +44,36 @@ const menuClose = () => {
 //     });
 //   });
 
-}
-close.addEventListener('click',menuClose);
+// }
+// close.addEventListener('click',menuClose);
 
-$(function(){
-    $('.bxslider').bxSlider({
-      // オプションを書く場所
-    });
+$(document).ready(function(){
+  $('.slider4').bxSlider({
+    slideWidth: 400,
+    minSlides: 2,
+    maxSlides: 3,
+    moveSlides: 1,
+    slideMargin: 10
   });
+});
 
-  $(function(){
-    $('.bxslider2').bxSlider({
-      // オプションを書く場所
-    });
-  });
+// $(function(){
+//     $('.bxslider').bxSlider({
+//       mode: 'fade',
+//       captions: true,
+//       auto: true,
+//       speed: 1500,
+//       pause: 4000,
+//     });
+//   });
+
+
+  // $(function(){
+  //   $('.ye2').bxSlider({
+  //     mode: 'fade',
+  //     captions: true,
+  //     auto: true,
+  //     speed: 1500,
+  //     pause: 4000,
+  //   });
+  // });
